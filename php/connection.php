c39bb3acd56746c77566f9fb6edabb27af2de50e
Naoman Ali <112874876+naoman-17@users.noreply.github.com>
2023-01-25 13:54:56 +0530

    Add files via upload

--- php/connection.php
+++ php/connection.php
@@ -1,8 +1,8 @@
 <?php
     $servername ="localhost";
-    $username   ="root"
-    $password   =""
-    $dbname     ="login"
+    $username   ="root";
+    $password   ="";
+    $dbname     ="login";
 
     $conn=mysqli_connect($servername,$username,$password,$dbname);
 ?>
--- php/index.php
+++ php/index.php
@@ -1,7 +1,24 @@
 <?php
 
     include("connection.php");
+    $email=$_GET["email"];
+    $pass=$_GET["pass"];
 
-    
+    $query="select * from login where email='$email' && pass='$pass'";
+    $data=mysqli_query($conn,$query);
+    $total=mysqli_num_rows($data);
+    if($total==1)
+    {
+        header('Location:../welcome.html');
+    }
+    else
+    {
+        $alert=
+        "<script>
+            alert('WRONG USERNAME OR PASSWORD');
+            window.location.href='../index.html';
+        </script>";
+        echo $alert;
+    }
 
 ?>
